98a551d17243b9cff83381a25264496a3f07d301
Mohammed Saud <md.saud020@gmail.com>
2023-05-21 03:35:33 +0530

    add sign to gd column

--- src/components/LeagueTable.tsx
+++ src/components/LeagueTable.tsx
@@ -20,22 +20,26 @@ const LeagueTable = () => {
             </thead>
 
             <tbody>
-                {clubs.map((c, i) => (
-                    <tr key={c.name}>
-                        <td>{i + 1}</td>
-                        <td>
-                            <Link href={`/fixtures/${c.name.replace(' ', '-')}`}>
-                                {c.name}
-                            </Link>
-                        </td>
-                        <td>{c.played}</td>
-                        <td>{c.won}</td>
-                        <td>{c.drawn}</td>
-                        <td>{c.lost}</td>
-                        <td>{c.goalsScored - c.goalsConceded}</td>
-                        <td>{c.points}</td>
-                    </tr>
-                ))}
+                {clubs.map((c, i) => {
+                    const gd = c.goalsScored - c.goalsConceded;
+
+                    return (
+                        <tr key={c.name}>
+                            <td>{i + 1}</td>
+                            <td>
+                                <Link href={`/fixtures/${c.name.replace(' ', '-')}`}>
+                                    {c.name}
+                                </Link>
+                            </td>
+                            <td>{c.played}</td>
+                            <td>{c.won}</td>
+                            <td>{c.drawn}</td>
+                            <td>{c.lost}</td>
+                            <td>{gd >= 0 ? `+${gd}` : gd}</td>
+                            <td>{c.points}</td>
+                        </tr>
+                    );
+                })}
             </tbody>
         </table>
     );
