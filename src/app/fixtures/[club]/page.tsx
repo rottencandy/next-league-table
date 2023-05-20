2b1392ea598f640bcc4c005bba7d12e182fdf5c1
Mohammed Saud <md.saud020@gmail.com>
2023-05-21 00:54:05 +0530

    add tables for past & future games

--- src/app/fixtures/[club]/page.tsx
+++ src/app/fixtures/[club]/page.tsx
@@ -1,10 +1,15 @@
-import { clubByName, getClubs } from "@/lib/club";
+import { clubByName, getClubs, isPast } from "@/lib/club";
 import Link from "next/link";
 import styles from './page.module.css'
+import { compareAsc } from "date-fns";
 
 export default function Layout ({ params }: { params: { club: string }}) {
     const { club: name } = params;
     const club = clubByName(name.replace('-', ' '));
+
+    const fixtures = club.fixtures.sort((f1, f2) => compareAsc(f1.date, f2.date));
+    const past = fixtures.filter(f => isPast(f.date));
+    const upcoming = fixtures.filter(f => !isPast(f.date));
 
     return (
         <>
@@ -15,6 +20,22 @@ export default function Layout ({ params }: { params: { club: string }}) {
             <main className={styles.main}>
                 <h1>Fixtures</h1>
                 <h2>{club.name}</h2>
+
+                <section>
+                    <table>
+                        <caption>Upcoming games</caption>
+                        <tbody>
+                        </tbody>
+                    </table>
+                </section>
+
+                <section>
+                    <table>
+                        <caption>Past games</caption>
+                        <tbody>
+                        </tbody>
+                    </table>
+                </section>
             </main>
         </>
     );
